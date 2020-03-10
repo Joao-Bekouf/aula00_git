acebf5f6068fda80735bd4e803380904a75a675a
Joao-Bekouf <joaobkf@gmail.com>
2020-03-09 23:13:17 -0300

    Criacao dos Sets

--- eclipse-workspace/Aula_00/src/Aluno.java
+++ eclipse-workspace/Aula_00/src/Aluno.java
@@ -10,4 +10,12 @@ public class Aluno {
 	public int getId() {
 		return this.id;
 	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
 }
