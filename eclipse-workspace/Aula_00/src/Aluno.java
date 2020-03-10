596af409c5f48b8ea46b72345573b90af758035c
Soosbr <matheusosoos@hotmail.com>
2020-03-09 23:26:43 -0300

    construtor sem this

--- eclipse-workspace/Aula_00/src/Aluno.java
+++ eclipse-workspace/Aula_00/src/Aluno.java
@@ -3,6 +3,11 @@ public class Aluno {
 	private String nome;
 	private int id;
 	
+	public Aluno(String nome, int id) {
+		nome = nome;
+		id = id;
+	}
+	
 	public String getNome() {
 		return this.nome;
 	}
